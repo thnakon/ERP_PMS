ddd159b815db97308722d3016023f152ff0f2d00
thnakon <thnakon.d@gmail.com>
2026-03-03 21:13:49 +0700

    Fix: JavaScript deleteRow error in Customers list by using CustomerPage object

--- resources/views/customers/index.blade.php
+++ resources/views/customers/index.blade.php
@@ -281,8 +281,8 @@
                                     </a>
                                     <div class="h-px bg-gray-100 my-1"></div>
                                     <button type="button"
-                                        onclick="deleteRow({{ $customer->id }}, '{{ $customer->name }}', '{{ route('customers.destroy', $customer) }}')"
-                                        class="ios-dropdown-item ios-dropdown-item-danger">
+                                        onclick="CustomerPage.deleteCustomer('{{ route('customers.destroy', $customer) }}')"
+                                        data-no-loading class="ios-dropdown-item ios-dropdown-item-danger">
                                         <i class="ph ph-trash ios-dropdown-icon"></i>
                                         <span>{{ __('delete') }}</span>
                                     </button>
@@ -323,6 +323,48 @@
 
 @push('scripts')
     <script>
-        // Customer page specific scripts
+        const CustomerPage = {
+            deleteUrl: null,
+
+            deleteCustomer(url) {
+                this.deleteUrl = url;
+                if (typeof toggleModal === 'function') {
+                    toggleModal(true, 'delete-modal');
+                }
+            },
+
+            executeDelete() {
+                if (!this.deleteUrl) return;
+
+                const form = document.createElement('form');
+                form.method = 'POST';
+                form.action = this.deleteUrl;
+
+                const csrfToken = document.querySelector('meta[name="csrf-token"]').content;
+                form.innerHTML = `
+                    <input type="hidden" name="_token" value="${csrfToken}">
+                    <input type="hidden" name="_method" value="DELETE">
+                `;
+
+                document.body.appendChild(form);
+                form.submit();
+            }
+        };
+
+        window.CustomerPage = CustomerPage;
+
+        // Override executeDelete for customers page (wait for app.js to finish setting it)
+        document.addEventListener('DOMContentLoaded', () => {
+            setTimeout(() => {
+                const originalExecuteDelete = window.executeDelete;
+                window.executeDelete = function() {
+                    if (CustomerPage.deleteUrl) {
+                        CustomerPage.executeDelete();
+                    } else if (typeof originalExecuteDelete === 'function') {
+                        originalExecuteDelete();
+                    }
+                };
+            }, 50);
+        });
     </script>
 @endpush
